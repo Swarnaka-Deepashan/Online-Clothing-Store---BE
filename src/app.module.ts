7cccef5862703191f68f26587263aa91687f7c3b
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-10 20:36:38 +0530

    feat(item): initialize item controller, module, service with CRUD endpoints step 02

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
+import { ItemController } from './item/item.controller';
 
 @Module({
   imports: [ItemModule],
-  controllers: [AppController],
+  controllers: [AppController, ItemController],
   providers: [AppService],
 })
 export class AppModule {}
