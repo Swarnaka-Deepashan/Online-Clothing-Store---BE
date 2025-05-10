f60c7f03f36053cf156615c1167be21d94560c1a
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-11 01:00:33 +0530

    refactor(item): Modify create-item DTO

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,23 @@
 export class CreateItemDto {
   name: string;
-  category: number;
-  size: string;
+  description: string;
+  category: string;
+  subCategory: string;
+  brand: string;
+  sizeOptions: string[];
+  colorOptions: string[];
+  material: string;
+  price: number;
+  discountPrice: string;
+  // currency: string;
+  stock: number;
+  isAvailable: boolean;
+  images: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  genderTarget: string;
+  // ratings: number;
+  tags: string[];
+  sku: string;
+  deliveryInfo: string;
 }
