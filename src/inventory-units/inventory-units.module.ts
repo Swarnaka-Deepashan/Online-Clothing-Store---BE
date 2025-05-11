9bc0f830faae0edd01535f5e47cee48cd9bba3e9
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-11 10:41:52 +0530

    feat(inventory): Implement inventory unit schema step 02

--- src/inventory-units/inventory-units.module.ts
+++ src/inventory-units/inventory-units.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { InventoryUnitsService } from './inventory-units.service';
 import { InventoryUnitsController } from './inventory-units.controller';
+import {
+  InventoryUnit,
+  InventoryUnitSchema,
+} from 'src/schemas/inventory-unit.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: InventoryUnit.name, schema: InventoryUnitSchema },
+    ]),
+  ],
   controllers: [InventoryUnitsController],
   providers: [InventoryUnitsService],
 })
--- src/inventory-units/inventory-units.service.ts
+++ src/inventory-units/inventory-units.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateInventoryUnitDto } from './dto/create-inventory-unit.dto';
 import { UpdateInventoryUnitDto } from './dto/update-inventory-unit.dto';
+import { InventoryUnit } from 'src/schemas/inventory-unit.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class InventoryUnitsService {
+  constructor(
+    @InjectModel(InventoryUnit.name)
+    private inventoryUnitModel: Model<InventoryUnit>,
+  ) {}
+
   create(createInventoryUnitDto: CreateInventoryUnitDto) {
-    return 'This action adds a new inventoryUnit';
+    const createdInventoryUnit = new this.inventoryUnitModel(
+      createInventoryUnitDto,
+    );
+    return createdInventoryUnit.save();
   }
 
   findAll() {
