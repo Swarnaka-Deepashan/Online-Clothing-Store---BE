8e9d4753c65a95928fbbb7b5c00bd0b01bfabd70
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-11 10:10:23 +0530

    feat(inventory): Implement create-inventory-unit DTO

--- src/inventory-units/dto/create-inventory-unit.dto.ts
+++ src/inventory-units/dto/create-inventory-unit.dto.ts
@@ -1,1 +1,12 @@
-export class CreateInventoryUnitDto {}
+export class CreateInventoryUnitDto {
+  itemId: string;
+  size: string;
+
+  color: string;
+  purchasePrice: number;
+
+  sellingPrice: number;
+  batchNumber: string;
+
+  location: string;
+}
