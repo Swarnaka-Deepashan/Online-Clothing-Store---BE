debbb23e317833a59acb51c9aed7cd3e89499cc4
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-11 10:01:10 +0530

    feat(clothing-size): implement clothing-size enum

---
+++ src/enums/clothing-size.enum.ts
@@ -0,0 +1,14 @@
+export enum ClothingSize {
+  XS = 'XS',
+  S = 'S',
+  M = 'M',
+  L = 'L',
+  XL = 'XL',
+  XXL = 'XXL',
+  XXXL = 'XXXL',
+  NB = 'NB',
+  M3 = '3M',
+  M6 = '6M',
+  M9 = '9M',
+  M12 = '12M',
+}
