f4e8c7ffad1603f2381f7599f16b06716bf0323b
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-14 03:59:07 +0530

    feat(order): Implement create order DTO

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,27 @@
-export class CreateOrderDto {}
+import { ObjectId } from 'mongoose';
+
+export class productsRequest {
+  itemId: ObjectId;
+
+  itemName: string;
+
+  selectedSize: string;
+
+  selectedColor: string;
+
+  quantity: number;
+
+  basePrice: number;
+}
+
+export class CreateOrderDto {
+  userId: ObjectId;
+
+  productsRequests: productsRequest[];
+
+  shippingAddress: string;
+
+  paymentMethod: string;
+
+  paidAmount: string;
+}
